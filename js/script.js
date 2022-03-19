5f8d6273ad46fe02d9cf8fc70cab25437cb03d10
Roman <laxus1991@ukr.net>
2022-03-19 21:51:08 +0200

    add script for product and info

--- js/script.js
+++ js/script.js
@@ -23,3 +23,46 @@ openMap.addEventListener("click", function () {
 closeMap.addEventListener("click", function () {
         maps.classList.remove("map-popup"); 
 });
+
+
+let deliveryButton = document.querySelector('.delivery-button');
+let guaranteeButton = document.querySelector('.guarantee-button');
+let creditButton = document.querySelector('.credit-button');
+let activeServiceButton = document.querySelector('.active');
+
+let infoDelivery = document.querySelector('.service-delivery');
+let infoGuarantee = document.querySelector('.service-guarantee');
+let infoCredit = document.querySelector('.service-credit');
+let infoClose = document.querySelector('.service-info-close');
+
+
+deliveryButton.addEventListener("click", function () {
+    deliveryButton.classList.add("active");
+    guaranteeButton.classList.remove("active");
+    creditButton.classList.remove("active");
+
+    infoDelivery.classList.remove("service-info-close");
+    infoGuarantee.classList.add("service-info-close");
+    infoCredit.classList.add("service-info-close");
+})
+
+guaranteeButton.addEventListener("click", function () {
+    guaranteeButton.classList.add("active");
+    deliveryButton.classList.remove("active");
+    creditButton.classList.remove("active");
+
+    infoGuarantee.classList.remove("service-info-close");
+    infoDelivery.classList.add("service-info-close");
+    infoCredit.classList.add("service-info-close");
+})
+
+creditButton.addEventListener("click", function () {
+    creditButton.classList.add("active");
+    deliveryButton.classList.remove("active");
+    guaranteeButton.classList.remove("active");
+
+    infoCredit.classList.remove("service-info-close");
+    infoGuarantee.classList.add("service-info-close");
+    infoDelivery.classList.add("service-info-close");
+})
+
