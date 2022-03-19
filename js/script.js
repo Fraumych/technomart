da440e0b75eefd64bbacd41acd917ce09da75b4b
Roman <laxus1991@ukr.net>
2022-03-19 20:29:51 +0200

    change index, js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,25 @@
 
-let close = document.querySelector('.contact-close')
-let open = document.querySelector('.button-contacts')
+let contacts = document.querySelector('.contacts-popup',);
+let openContacts = document.querySelector('.button-contacts');
+let closeContacts = document.querySelector('.button-close');
 
-open.addEventListener("click", function () {
-    if (close.classList.contains("contact-close")) {
-        close.classList.remove("contact-close");
-        close.classList.add("contact-popup")
-    }
-}
-)
+openContacts.addEventListener("click", function () {
+        contacts.classList.add("contact-popup")
+});
 
-let back = document.querySelector('.button-close');
+closeContacts.addEventListener("click", function () {
+        contacts.classList.remove("contact-popup");
+});
 
-back.addEventListener("click", function () {
-    if (close.classList.contains("contact-popup")) {
-        close.classList.remove("contact-popup");
-        close.classList.add("contact-close")
-    }
-}
-)
 
-let closeMap = document.querySelector('.map-close');
+let maps = document.querySelector('.maps-popup');
 let openMap = document.querySelector('.map-open');
+let closeMap = document.querySelector('.button-map-close');
 
 openMap.addEventListener("click", function () {
-    if (closeMap.classList.contains("map-close")) {
-        closeMap.classList.remove("map-close");
-        closeMap.classList.add("map-popup")
-    }
-}
-)
+        maps.classList.add("map-popup")
+});
 
-let backMap = document.querySelector('.button-map-close');
-
-backMap.addEventListener("click", function () {
-    if (closeMap.classList.contains("map-popup")) {
-        closeMap.classList.remove("map-popup");
-        closeMap.classList.add("map-close")
-    }
-}
-)
+closeMap.addEventListener("click", function () {
+        maps.classList.remove("map-popup"); 
+});
